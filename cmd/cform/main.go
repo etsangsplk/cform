6dd5a13f9e03c6382963018662785f168be2601b
isubuz <isubuz.g@gmail.com>
2017-01-07 22:58:01 +0000

    Adds support for debug flag

--- cmd/cform/main.go
+++ cmd/cform/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 var rootCmdFlags struct {
+	debug         bool
 	tmplSrc       string
 	tmplOut       string
 	tmplOverwrite bool
@@ -17,13 +18,17 @@ var rootCmdFlags struct {
 var rootCmd = &cobra.Command{
 	Use:   "cform",
 	Short: "CloudFormation utility which provides Terraform-like CLI functionalities",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if rootCmdFlags.debug {
+			log.SetLevel(log.DebugLevel)
+		}
+	},
 }
 
 func main() {
-	log.SetLevel(log.DebugLevel)
-
-	rootCmd.PersistentFlags().StringVar(&rootCmdFlags.tmplSrc, "template-src", "templates", "Directory containing CloudFormation templates")
+	rootCmd.PersistentFlags().BoolVar(&rootCmdFlags.debug, "debug", false, "Print debug information")
 	rootCmd.PersistentFlags().StringVar(&rootCmdFlags.tmplOut, "template-out", "", "Location to which the merged template will be written")
+	rootCmd.PersistentFlags().StringVar(&rootCmdFlags.tmplSrc, "template-src", "templates", "Directory containing CloudFormation templates")
 	rootCmd.PersistentFlags().BoolVar(&rootCmdFlags.tmplOverwrite, "template-overwrite", false, "Overwrite existing template output file")
 
 	if err := rootCmd.Execute(); err != nil {
